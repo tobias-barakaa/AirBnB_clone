1be1f3c35e8afe703f7e8e9364f765e85e60b633
tobias-barakaa <tobiasbarakan@gmail.com>
2023-07-15 02:59:34 -0700

    dey-plaly

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,22 @@
-sjfkfi
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+    
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
