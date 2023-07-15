c6eed42df715ebf33d64881f150e5961d79db5ef
tobias-barakaa <tobiasbarakan@gmail.com>
2023-07-15 09:55:50 -0700

    models

--- models/base_model.py
+++ models/base_model.py
@@ -1,78 +1,34 @@
-#!/usr/bin/env python3
-"""
-   BaseModel that defines all common attributes/methods for other classes:
-"""
-from datetime import datetime
+#!/usr/bin/python3
 import uuid
-import models 
+from datetime import datetime
+import models
+
 
 class BaseModel:
-    """
-    A base model class that defines common attributes and methods for other classes.
-
-    Attributes:
-        id (str): Unique identifier assigned to an instance (UUID).
-        created_at (datetime): Date and time when the instance was created.
-        updated_at (datetime): Date and time when the instance was last updated.
-
-    Methods:
-        __str__(): Returns a string representation of the object.
-        save(): Updates the `updated_at` attribute with the current datetime.
-        to_dict(): Converts the object to a dictionary representation.
-
-    """
-
-    def __init__(self):
-        """
-        Initializes a new instance of the BaseModel class.
-
-        This constructor sets the `id` attribute to a unique identifier (UUID),
-        `created_at` attribute to the current datetime, and `updated_at` attribute
-        to the same value.
-
-        """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
-        """
-        Returns a string representation of the object.
-
-        The string format is: "[<class name>] (<id>) <attribute dictionary>"
-
-        Returns:
-            str: String representation of the object.
-
-        """
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """
-        Updates the `updated_at` attribute with the current datetime.
-
-        This method should be called whenever an object is modified.
-
-        """
-        self.updated_at = datetime.utcnow()
-        models.storage.new(self)
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """
-        Converts the object to a dictionary representation.
-
-        This method returns a dictionary containing all the instance attributes.
-        It also adds the '__class__' key with the class name, and converts the
-        'created_at' and 'updated_at' attributes to string objects in ISO format.
-
-        Returns:
-            dict: Dictionary representation of the object.
-
-        """
         data = self.__dict__.copy()
         data['__class__'] = self.__class__.__name__
         data['created_at'] = self.created_at.isoformat()
         data['updated_at'] = self.updated_at.isoformat()
         return data
+
