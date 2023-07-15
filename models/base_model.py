fbbb9cb57fcf535900e3b1630add799ac8580dd2
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-15 20:02:43 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -15,14 +15,14 @@ class BaseModel:
                 setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
+            self.created_at = datetime.today()
             self.updated_at = self.created_at
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
