6b6b8163f250cd758ecf5bccd7677e11a37f8625
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-15 17:40:48 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,54 +1,44 @@
 #!/usr/bin/python3
 """
-fun class base model do everuthing
+This is the base model that contains serial/deserial information
 """
-import uuid
 from datetime import datetime
-import models
+import uuid
+from models import storage
 
 
-class BaseModel:
-    """
-    our code insert here
-    """
+class BaseModel():
+    """ Defines all common attributes/methods for other classes """
     def __init__(self, *args, **kwargs):
-        """
-        constructor of base Model
-        """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+        """ Initializes the instances attributes """
+        if kwargs:
+            date_format = "%Y-%m-%dT%H:%M:%S.%f"
+            k_dict = kwargs.copy()
+            del k_dict["__class__"]
+            for key in k_dict:
+                if (key == "created_at" or key == "updated_at"):
+                    k_dict[key] = datetime.strptime(k_dict[key], date_format)
+            self.__dict__ = k_dict
         else:
-            for key, val in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    dataTime = "%Y-%m-%dT %H:%M:%S.%f"
-                    val = datetime.strptime(kwargs[key], dataTime)
-                if key != '__class__':
-                    setattr(self, key, val)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            storage.new(self)
 
     def __str__(self):
-        """
-        method for named
-        """
-        nameClass = self.__class__.__name__
-        return ("[{}] ({}) {}".format(nameClass, self.id, self.__dict__))
+        """ Prints object in friendly format"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """
-        method for save stuff
-        """
-        self.updated_at = datetime.now()
-        models.storage.save()
+        """ Updates update_at """
+        self.updated_at = datetime.today()
+        storage.save()
 
     def to_dict(self):
-        """
-        method for create a dict
-        """
-        new_dict = dict(self.__dict__)
+        """ Generate a new dict with an extra field __class__ """
+        new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
-        formatTime = "%Y-%m-%dT %H:%M:%S.%f"
-        new_dict["created_at"] = self.created_at.strftime(formatTime)
-        new_dict["updated_at"] = self.updated_at.strftime(formatTime)
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
