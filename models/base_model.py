99a8fead653cc1773b1623d88ed0f52b189659da
tobias-barakaa <tobiasbarakan@gmail.com>
2023-07-15 03:03:35 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,30 @@
+import json
 import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-    
-    def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            self.__dict__ = kwargs
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs.get("created_at"), "%Y-%m-%dT%H:%M:%S.%f")
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs.get("updated_at"), "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
 
     def save(self):
         self.updated_at = datetime.now()
 
-    def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+    def to_json(self):
+        model_dict = self.__dict__.copy()
+        model_dict["__class__"] = type(self).__name__
+        for key, value in model_dict.items():
+            if isinstance(value, datetime):
+                model_dict[key] = value.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        return model_dict
+
+    def __str__(self):
+        return "[{0}] ({1}) {2}".format(self.__class__.__name__, self.id, self.__dict__)
 
