1a580fc6555e22242999cec4937259ae44c2ce6f
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-15 14:25:14 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -27,16 +27,3 @@ class BaseModel:
         data['created_at'] = self.created_at.isoformat()
         data['updated_at'] = self.updated_at.isoformat()
         return data
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
