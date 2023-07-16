34c98dcf3952442667dfbba3d4fdf3eb57b0151e
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-17 00:16:56 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,42 @@
+"""
+This module defines a storage mechanism for model objects.
+
+Classes:
+    Storage: A storage class for managing model objects.
+
+Module-level Variables:
+    storage: An instance of the Storage class for easy access.
+
+Usage:
+    from models.base_model import BaseModel
+    from storage import storage
+
+    my_model = BaseModel()
+    storage.new(my_model)  # Add the model to the storage
+    storage.save()  # Save the changes made to the stored models
+"""
+
 from models.base_model import BaseModel
 
 
 class Storage:
-    def new(self, obj):
-        # Your implementation to store the object goes here
+    """
+    The Storage class provides a mechanism to store and manage model objects.
+    """
+    THIS = self
+    def new(THIS, obj):
+        """
+        Add a model object to the storage.
+
+        Args:
+            obj: The model object to be added to the storage.
+        """
         pass
 
-    def save(self):
-        # Your implementation to save objects goes here
+    def save(THIS):
+        """
+        Save the changes made to the stored model objects.
+        """
         pass
 
 
