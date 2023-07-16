9a51971eaa4bc27a64cd6fdd0d4a375a960a4b32
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-16 23:01:31 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -29,6 +29,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         class_name = self.__class__.__name__
