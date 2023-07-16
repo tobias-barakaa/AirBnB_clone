54a5e159c9b7996efc16d325bdcebdf403f9d015
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-16 19:46:15 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -22,30 +22,22 @@ class BaseModel:
 
     """
 
-    def __init__(self, *args, **kwargs):
-        """Public instance artributes initialization
-        after creation
+    def __init__(self, **kwargs):
+        """
+        Initializes a new instance of the BaseModel class.
 
         Args:
-            *args(args): arguments
-            **kwargs(dict): attrubute values
+            **kwargs: Keyword arguments to set instance attributes.
+                      Keys can be attribute names and values are corresponding values.
 
         """
-        DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
-        if not kwargs:
-            self.id = str(uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-            models.storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if key in ("updated_at", "created_at"):
-                    self.__dict__[key] = datetime.strptime(
-                        value, DATE_TIME_FORMAT)
-                elif key[0] == "id":
-                    self.__dict__[key] = str(value)
-                else:
-                    self.__dict__[key] = value
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
     def __str__(self):
         """
         Returns a string representation of the object.
