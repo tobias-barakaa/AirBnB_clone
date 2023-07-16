084be2799ad078d7ba35d656e593e36895dca541
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-17 00:02:51 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel:
                 if key != "__class__":
                     setattr(self, key, value)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             models.storage.new(self)
