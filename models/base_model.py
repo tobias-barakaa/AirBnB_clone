64e81321a40922ab6d6cb0ca9924a56f6caa696a
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-16 23:05:55 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
@@ -29,7 +30,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         class_name = self.__class__.__name__
@@ -37,7 +37,6 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
