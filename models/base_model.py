ea4992cd66bdc1cedd15b374f43c861215375ca5
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-16 20:17:36 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -34,19 +34,15 @@ class BaseModel:
                       Keys can be attribute names and
                       values are corresponding values.
         """
+        self.id = str(uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
         if kwargs:
             for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
                 if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value))
-                else:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-
     def __str__(self):
         """
         Returns a string representation of the object.
