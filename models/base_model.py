15b51199bfde8fe873bbb4f4cb687053c27cc9c6
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-16 22:59:59 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,20 +4,43 @@ import uuid
 from datetime import datetime
 import models
 
-class BaseModel(object):
+class BaseModel:
     def __init__(self, *args, **kwargs):
-        super(BaseModel, self).__init__()
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-        for key, value in kwargs.items():
-            if key != "__class__":
-                setattr(self, key, value)
+        if kwargs:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            else:
+                self.id = str(uuid.uuid4())
+
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.created_at = datetime.now()
+
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.updated_at = datetime.now()
+
+            for key, value in kwargs.items():
+                if key not in ["id", "created_at", "updated_at"]:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+        models.storage.save()
+
     def to_dict(self):
-        d = {}
-        for key, value in self.__dict__.items():
-            if key != "__class__":
-                d[key] = value
-        return d
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
