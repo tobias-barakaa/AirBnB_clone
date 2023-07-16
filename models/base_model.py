1a8979ee856a43b80b073301f3eedb4ad402ab46
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-16 16:33:33 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel:
 
     """
 
-    def __init__(self,*args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
 
@@ -34,7 +34,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-        
+
         for key, value in kwargs.items():
             setattr(self, key, value)
 
