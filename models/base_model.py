b67e16adcb4347af3986ec8e810c60e2e1d76cfa
tobias-barakaa <tobiasbarakan@gmail.com>
2023-07-16 03:50:26 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -81,7 +81,9 @@ class BaseModel:
 
         """
         obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        if 'created_at' in obj_dict:
+            obj_dict['created_at'] = obj_dict['created_at'].isoformat()
+            if 'updated_at' in obj_dict:
+                obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
+                obj_dict['__class__'] = self.__class__.__name__
+                return obj_dict
