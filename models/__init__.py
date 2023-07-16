f8c0031cfb0606e519e2110565a52f516b6178de
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-17 02:13:11 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,47 +1,43 @@
-# """
-# This module defines a storage mechanism for model objects.
+"""
+This module defines a storage mechanism for model objects.
 
-# Classes:
-#     Storage: A storage class for managing model objects.
+Classes:
+    Storage: A storage class for managing model objects.
 
-# Module-level Variables:
-#     storage: An instance of the Storage class for easy access.
+Module-level Variables:
+    storage: An instance of the Storage class for easy access.
 
-# Usage:
-#     from models.base_model import BaseModel
-#     from storage import storage
+Usage:
+    from models.base_model import BaseModel
+    from storage import storage
 
-#     my_model = BaseModel()
-#     storage.new(my_model)  # Add the model to the storage
-#     storage.save()  # Save the changes made to the stored models
-# """
+    my_model = BaseModel()
+    storage.new(my_model)  # Add the model to the storage
+    storage.save()  # Save the changes made to the stored models
+"""
 
-# from models.base_model import BaseModel
-# import models.engine.file_storage
+from models.base_model import BaseModel
+import models.engine.file_storage
 
 
-# class Storage:
-#     """
-#     The Storage class provides a mechanism to store and manage model objects.
-#     """
-#     def new(self, obj):
-#         """
-#         Add a model object to the storage.
+class Storage:
+    """
+    The Storage class provides a mechanism to store and manage model objects.
+    """
+    def new(self, obj):
+        """
+        Add a model object to the storage.
 
-#         Args:
-#             obj: The model object to be added to the storage.
-#         """
-#         pass
+        Args:
+            obj: The model object to be added to the storage.
+        """
+        pass
 
-#     def save(self):
-#         """
-#         Save the changes made to the stored model objects.
-#         """
-#         pass
-# storage = Storage()
-# storage = models.engine.file_storage.FileStorage()
-# storage.reload()
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
+    def save(self):
+        """
+        Save the changes made to the stored model objects.
+        """
+        pass
+storage = Storage()
+storage = models.engine.file_storage.FileStorage()
 storage.reload()
