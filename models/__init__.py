b93860a805a781f733503f961a688b5e9b13d898
tobias-barakaa <tobiasbarakan@gmail.com>
2023-07-16 13:05:05 -0700

    init

---
+++ models/__init__.py
@@ -0,0 +1,14 @@
+from models.base_model import BaseModel
+
+
+class Storage:
+    def new(self, obj):
+        # Your implementation to store the object goes here
+        pass
+
+    def save(self):
+        # Your implementation to save objects goes here
+        pass
+
+
+storage = Storage()
