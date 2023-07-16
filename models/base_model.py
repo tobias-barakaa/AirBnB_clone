1393dc159cfa9481f0067c5fd2985916e31afffd
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-16 20:19:28 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -34,15 +34,19 @@ class BaseModel:
                       Keys can be attribute names and
                       values are corresponding values.
         """
-        self.id = str(uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
         if kwargs:
             for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
+                    setattr(self, key, datetime.strptime(value))
+                else:
                     setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
+
     def __str__(self):
         """
         Returns a string representation of the object.
@@ -68,7 +72,7 @@ class BaseModel:
         This method should be called whenever an object is modified.
 
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
