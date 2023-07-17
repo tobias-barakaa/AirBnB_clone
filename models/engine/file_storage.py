abc1018db9a72ea6650e38d2aa03e7656695ed7f
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-17 03:47:19 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,78 +1,44 @@
 #!/usr/bin/python3
-"""Basemodel class func"""
-import uuid
-from datetime import datetime
-import models
+"""
+json serioulize/deserialize
+"""
 
 
-class BaseModel:
-    """
-    A base model class that defines common attributes and methods for other classes.
+import json
+from models.base_model import BaseModel
 
-    Attributes:
-        id (str): Unique identifier assigned to an instance (UUID).
-        created_at (datetime): Date and time when the instance was created.
-        updated_at (datetime): Date and time when the instance was last updated.
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
 
-    Methods:
-        __init__(**kwargs): Initializes a new instance of the BaseModel class.
-        __str__(): Returns a string representation of the object.
-        save(): Updates the `updated_at` attribute with the current datetime and saves the instance.
-        to_dict(): Returns a dictionary representation of the object.
+    def all(self):
+        """Return the dictionary __objects."""
+        return self.__objects
 
-    """
-
-    def __init__(self, **kwargs):
-        """
-        Initializes a new instance of the BaseModel class.
-
-        Args:
-            **kwargs: Keyword arguments to set instance attributes.
-                      Keys can be attribute names and values are corresponding values.
-
-        """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-    def __str__(self):
-        """
-        Returns a string representation of the object.
-
-        The string format is: "[<class name>] (<id>) <attribute dictionary>"
-
-        Returns:
-            str: String representation of the object.
-
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    def new(self, obj):
+        """Set in __objects the obj with key <obj class name>.id."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
+        """Serialize __objects to the JSON file (path: __file_path)."""
+        data = {}
+        for key, obj in self.__objects.items():
+            data[key] = obj.to_dict()
+        with open(self.__file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file)
+
+    def reload(self):
         """
-        Updates the `updated_at` attribute with the current datetime and saves the instance.
-
-        This method should be called whenever an object is modified.
-
+        Deserialize the JSON file to __objects (only if the JSON file exists).
+        If the file doesn’t exist, no exception should be raised.
         """
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        """
-        Returns a dictionary representation of the object.
-
-        This method returns a dictionary containing all the instance attributes,
-        including the '__class__' key with the class name. The 'created_at' and
-        'updated_at' attributes are converted to string objects in the ISO format.
-
-        Returns:
-            dict: Dictionary representation of the object.
-
-        """
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split(".")
+                    obj = globals()[class_name](**value)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
