69085cc638753610b3e7d8125c55a7c011d35b7d
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-17 05:53:03 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 """
-classamenity
+class amenity
 """
-
 from models.base_model import BaseModel
 
-class Amenity(BaseModel):
-    """Amenity class that inherits from BaseModel"""
-    name = ""
 
-    def __init__(self, *args, **kwargs):
-        """Initializes Amenity instance"""
-        super().__init__(*args, **kwargs)
+class Amenity(BaseModel):
+    """inheritated class Amenity from BaseModel"""
+    name = ""
