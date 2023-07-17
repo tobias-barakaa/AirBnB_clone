69e33a3f5e64ac70feeecf7169b8e1e0c5fa3277
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-17 04:40:44 +0300

    Update console.py

--- console.py
+++ console.py
@@ -117,8 +117,9 @@ class HBNBCommand(cmd.Cmd):
                     attr_value = eval(attr_value)
                 except (NameError, SyntaxError):
                     pass
-                setattr(obj, attr_name, attr_value)
-                obj.save()
+                if attr_name not in ["id", "created_at", "updated_at"]:
+                    setattr(obj, attr_name, attr_value)
+                    obj.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
