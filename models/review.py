5ff6ceb49a4f80b7bd3c41d43edeed918e781117
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-17 05:54:29 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """
-class review
+class review incated place and user and text
 """
-
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
-    """Review class that inherits from BaseModel"""
+    """inheritated class Review from BaseModel"""
     place_id = ""
     user_id = ""
     text = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initializes Review instance"""
-        super().__init__(*args, **kwargs)
