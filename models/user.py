c51a5e04181bdf287589a563acf212c9f47a9bcf
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-17 05:42:05 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -4,11 +4,14 @@ User Module
 """
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
-    """
-    User class that inherits from BaseModel
-    """
+    """User class that inherits from BaseModel"""
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes User instance"""
+        super().__init__(*args, **kwargs)
