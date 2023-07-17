faece404e6be371257465a72269587be13c34d60
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-07-17 05:57:25 +0300

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,1 +1,20 @@
-fj
+#!/usr/bin/python3
+"""
+class place for general stuff
+"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """inheritated class Place from BaseModel"""
+    name = ""
+    city_id = ""
+    user_id = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
